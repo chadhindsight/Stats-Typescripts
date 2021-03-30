3807651bf09e405a048bf09618463eced18219b0
Chad Hinds <chadrickj8@gmail.com>
2021-03-30 13:29:52 -0400

    Determine what type of content we expect the file to have

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 const matches = fs.readFileSync('football.csv', {
-    //Determine what type of content we expect the file to have
+    //Determine what type of content we expect the file to have.
     encoding: 'utf-8'
 });
 console.log(matches)
