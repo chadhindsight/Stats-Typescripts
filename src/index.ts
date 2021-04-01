d081e65445e08fdbe8a61e570ecfc8e9a21facfb
Chad Hinds <chadrickj8@gmail.com>
2021-04-01 10:34:55 -0400

    Javascript style approach that gets the amount of games a specific team won

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,21 @@ import fs from 'fs';
 const matches = fs.readFileSync('football.csv', {
     //Determine what type of content we expect the file to have
     encoding: 'utf-8'
-});
-console.log(matches)
+}).split('\n')
+    .map((row: string): string[] => {
+        return row.split(',')
+    })
+
+let manUnitedWins = 0
+
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++
+    }
+
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++
+    }
+}
+
+console.log(`Man United won ${manUnitedWins} games!`)
