5254fb4c79e7d6a0978f798038af425fae6b38a9
Chad Hinds <chadrickj8@gmail.com>
2021-04-03 10:34:45 -0400

    Import csv file reader class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs.readFileSync('football.csv', {
-    //Determine what type of content we expect the file to have
-    encoding: 'utf-8'
-}).split('\n')
-    .map((row: string): string[] => {
-        return row.split(',')
-    })
-
+const reader = new CsvFileReader('football.csv');
+reader.read()
 //Enum is an object that stores closely related values.
 enum MatchResult {
     HomeWin = 'H',
@@ -17,7 +11,7 @@ enum MatchResult {
 
 let manUnitedWins = 0
 
-for (let match of matches) {
+for (let match of reader.data) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++
     }
